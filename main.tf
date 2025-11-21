d27b16d3e7cde288e6df2a60723dec699864a7a3
caleb_ibm <caleb.ooi@ibm.com>
2025-11-21 16:32:54 +1100

    reduce vm

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_route_table_association" "public" {
 resource "aws_instance" "web" {
   ami                         = "ami-0e1a3a59369c81682" 
   instance_type               = "t3.medium"
-  count                       = "2"
+  count                       = "1"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
